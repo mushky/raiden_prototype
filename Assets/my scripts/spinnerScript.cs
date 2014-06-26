607b1bd280f46e15138373f33f19ddc34e73eaac
Ali Zaidi <msazaidi@gmail.com>
2014-06-25 19:46:13 -0700

    commit all the things

---
+++ Assets/my scripts/spinnerScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spinnerScript : MonoBehaviour 
+{
+	public Vector3 eulerAngleVelocity = new Vector3(0, 100, 0); // declare public variable
+
+	void FixedUpdate() // rigidbody rotation 
+	{
+		Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
+		rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+	}
+}
